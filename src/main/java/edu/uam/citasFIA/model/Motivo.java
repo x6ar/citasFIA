5749f6ce2326dbf10b405a4650d9d4488331a386
arnia <arni.arroliga8@gmail.com>
2023-11-25 15:28:33 -0600

    pedilo

--- src/main/java/edu/uam/citasFIA/model/Motivo.java
+++ src/main/java/edu/uam/citasFIA/model/Motivo.java
@@ -2,9 +2,15 @@ package edu.uam.citasFIA.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.openxava.annotations.ListProperties;
+import org.openxava.annotations.ReadOnly;
+import org.openxava.annotations.TextArea;
 import org.openxava.model.Identifiable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.Collection;
 
 @Entity
 @Getter
@@ -12,4 +18,11 @@ import javax.persistence.Entity;
 public class Motivo extends Identifiable {
     private String nombreMotivo;
     private Integer duracion;
+    @ReadOnly
+    @OneToMany(mappedBy = "motivo", cascade = CascadeType.REMOVE)
+    @ListProperties("fecha,estudiante.nombrecompletoe,motivo.nombreMotivo,motivo.duracion,observaciones")
+    Collection<Solicitud> solicitudes;
+
+    @TextArea
+    private String descripmot;
 }
--- src/main/java/edu/uam/citasFIA/model/Solicitud.java
+++ src/main/java/edu/uam/citasFIA/model/Solicitud.java
@@ -15,15 +15,20 @@ import java.util.Date;
 
 @Entity @Getter @Setter
 
+@View(members = "motivo;observaciones;personal;estudiante;fecha;" +
+        "horas,minutos;horaInicio,horaFin;estado;archivos")
 public class Solicitud extends Identifiable {
 
-   @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @DescriptionsList(descriptionProperties = "descripmot")
    private Motivo motivo;
 
- @ManyToOne(fetch = FetchType.LAZY)
- @DescriptionsList(descriptionProperties = "nombre")
-   private Bloques bloques;
 
+/*
+Observaciones. Hasta el final y poner place holder:
+Indique alguna otra observación adicional que se debería tener en cuenta
+para la cita con el Coordinador o Coordinadora
+ */
     @TextArea
     String observaciones;
 
@@ -44,6 +49,16 @@ public class Solicitud extends Identifiable {
     @DefaultValueCalculator(value = DefaultEstado.class)
     private Integer estado;
 
+ /*
+
+INGRESAR ... DE LA CITA
+horas : (LABEL) minutos MINUTOS (LABEL)
+
+VIEW HORA FIN
+LA CITA SE LLEVARA A CABO A LAS horaINICIO y finalizaria a las horaFIN
+el día: fecha
+
+*/
     @Column(length = 2)
     Integer horas;
 
@@ -51,17 +66,28 @@ public class Solicitud extends Identifiable {
     Integer minutos;
 
     @Depends("horas,minutos")
+    @Column(length = 5)
     public String getHoraInicio(){
-     if (horas != null && minutos !=null) return String.valueOf(horas) + ":" + String.valueOf(minutos);
+     if (horas != null && minutos !=null&& minutos>9) return String.valueOf(horas) + ":" + String.valueOf(minutos);
+     if(horas != null && minutos !=null&& minutos<=9) return String.valueOf(horas) + ":0" + String.valueOf(minutos);
      return "";
  }
+
  @Depends("horas,minutos,motivo.duracion")
- public String getHoraFin(){
-  if (horas != null && minutos !=null && motivo.getDuracion() != null) return String.valueOf(horas) + ":" + String.valueOf(minutos+ motivo.getDuracion());
+ @Column(length = 5)
+ public String getHoraFin() {
+  if (horas != null && minutos != null && motivo.getDuracion() != null) {
+   Integer totalMinutos = (horas * 60) + minutos + motivo.getDuracion();
+
+   Integer nuevasHoras = totalMinutos / 60;
+   Integer nuevosMinutos = totalMinutos % 60;
+
+   return String.valueOf(nuevasHoras) + ":" + String.valueOf(nuevosMinutos);
+  }
   return "";
  }
 
-
-
+ @Files
+ String archivos;
 
 }
